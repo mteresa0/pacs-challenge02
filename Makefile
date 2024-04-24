f7cdbef4a8de14f5a900ad5e1b0999fb6d77ee3c
Teresa Cazzola <mteresacazzola@gmail.com>
2024-04-24 23:13:41 +0200

    chrono utility added

--- Makefile
+++ Makefile
@@ -1,28 +1,31 @@
-CXX = g++
-CXXFLAGS += -std=c++20
-CPPFLAGS = -Wall -Wextra -Iinc
-
 INC_DIR = inc
 SRC_DIR = src
 OBJ_DIR = obj
+PACS_ROOT ?= /home/mteresa0/pacs/pacs-examples/Examples
+PACS_INC_DIR = $(PACS_ROOT)/include
+INCLUDE = -I$(INC_DIR) -I$(PACS_INC_DIR)
+
+CXX = g++
+CXXFLAGS += -std=c++20
+CPPFLAGS = -Wall -Wextra $(INCLUDE)
 
 SRCS = $(wildcard $(SRC_DIR)/*.cpp)
 HDRS = $(wildcard $(INC_DIR)/*.hpp)
 OBJS = $(patsubst $(SRC_DIR)/%.cpp, $(OBJ_DIR)/%.o, $(SRCS))
 EXEC = main
 
-all : main
+run : main
+	./main
 
-run : clean
+all : clean
 	make main
 	./main
 
 main: $(OBJS)
 	$(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OBJS) -o main
 
-
 $(OBJ_DIR)/%.o : $(SRC_DIR)/%.cpp
-	$(CXX) $(CXXFLAGS) -Iinc -c $< -o $@
+	$(CXX) $(CXXFLAGS) $(INCLUDE) -c $< -o $@
 
 boh : $(SRCS)
 	$(CXX) $(CXXFLAGS) $(CPPFLAGS) -o main $^
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 
+
 #include "Matrix_impl.hpp"
 #include "Operations.hpp"
 #include "Matrix.hpp"
+#include "chrono.hpp"
 #include <map>
 #include <iostream>
 
 using namespace algebra;
 
 int main() {
+
+    Timings::Chrono clock;
 
     std::map<std::array<std::size_t, 2>, double> map_mat;
     map_mat[{1,1}] = 2;
@@ -17,18 +21,44 @@ int main() {
 
     map_mat[{1,0}] += 29;
 
-    Matrix<double, algebra::COLS> m(map_mat, 5,8);
-    // m.compress();
-    m.verbose_print();
+    Matrix<double, algebra::ROWS> m(map_mat, 5,8);
 
+    //// compress matrix
+    m.compress(); m.uncompress();
+    clock.start();
+    m.compress();
+    clock.stop();
+    std::cout << "compress matrix: \n";
+    std::cout << clock << "\n";
+
+    //// uncompress matrix
+    m.uncompress();
+    m.compress();
+    clock.start();
+    m.uncompress();
+    clock.stop();
+    std::cout << "uncompress matrix: \n";
+    std::cout << clock << "\n";
+
+    m.compress();
+
+    //// multiplications
     std::vector<double> vec = {1,2,3,4,5};
+    auto res = m*vec; res = m*vec;
+    clock.start();
+    res = m*vec;
+    clock.stop();
+    std::cout << "compress SMV multiplication: \n";
+    std::cout << clock << "\n";
 
-
-    auto res = m*vec;
+    m.uncompress();
+    res = m*vec; res = m*vec;
+    clock.start();
+    res = m*vec;
+    clock.stop();
+    std::cout << "uncompress SMV multiplication: \n";
+    std::cout << clock << "\n";
     // res.compress();
-    // m.compress();
-
-    res.verbose_print();
 
 
     return 0;
